0d097d177f059daa4bf9d56cf6ae1d8d9e0bf2f4
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2022-12-03 12:24:15 +0200

    task-5

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const nameOutput = document.querySelector("span#name-output");
+
+const inputEl = document.querySelector("input#name-input");
+
+inputEl.addEventListener("input", (event) => {
+  nameOutput.textContent = event.target.value;
+});
