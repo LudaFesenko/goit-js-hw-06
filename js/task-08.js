f337395180f85b7ab4e9038f8affebff97e3d860
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2022-12-04 18:37:29 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector("form.login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const { email, password } = event.target.elements;
+  if (!email.value || !password.value) {
+    alert("Все поля должны быть заполнены");
+  } else {
+    console.log({ email: email.value, password: password.value });
+
+    event.target.reset();
+  }
+});
