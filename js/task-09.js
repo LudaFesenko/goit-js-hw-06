e08fe22f0486128f787a3a09fa67df1d2c137149
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2022-12-04 18:50:36 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector("button.change-color");
+
+const spanColor = document.querySelector("span.color");
+
+btnChangeColor.addEventListener("click", (event) => {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+
+  spanColor.textContent = color;
+});
